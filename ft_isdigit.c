9afc87241fb359ca75d73060979393224f8f1e0c
dishults <dmytroshults@gmail.com>
2018-10-25 16:00:45 +0200

    Update ft_isdigit.c

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -3,30 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dshults <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: dshults <dshults@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/14 18:19:06 by dshults           #+#    #+#             */
-/*   Updated: 2017/11/14 18:19:08 by dshults          ###   ########.fr       */
+/*   Updated: 2018/10/25 16:00:23 by dshults          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isdigit(int c)
 {
 	return (c >= '0' && c <= '9');
-}
-
-int	ft_isnumber(char *num)
-{
-	int	i;
-
-	i = 0;
-	if (!num || !num[i])
-		return (0);
-	while (num[i])
-	{
-		if (!ft_isdigit(num[i]))
-			return (0);
-		i++;
-	}
-	return (1);
 }
