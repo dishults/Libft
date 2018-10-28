c603c694cc9e74860c2211266e3303bf51675927
dishults <dmytroshults@gmail.com>
2018-10-28 13:10:39 +0100

    Added description to ft_strequ

--- ft_strequ.c
+++ ft_strequ.c
@@ -3,14 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strequ.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dshults <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: dshults <dshults@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/14 18:29:37 by dshults           #+#    #+#             */
-/*   Updated: 2017/11/14 18:29:39 by dshults          ###   ########.fr       */
+/*   Updated: 2018/10/28 13:10:17 by dshults          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+**	Lexicographical comparison between s1 and s2.
+**	If the 2 strings are identical the function returns 1, or 0 otherwise.
+*/
 
 int		ft_strequ(char const *s1, char const *s2)
 {
