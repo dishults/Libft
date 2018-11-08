cc02fd6f8aa1839a114bc402e9c9f410d27093fa
dishults <dmytroshults@gmail.com>
2018-11-08 11:57:11 +0100

    num_length

--- ft_printf/lib/ft_nbrconvert_base.c
+++ ft_printf/lib/ft_nbrconvert_base.c
@@ -3,41 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_nbrconvert_base.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dshults <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: dshults <dshults@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/28 18:04:45 by dshults           #+#    #+#             */
-/*   Updated: 2018/01/28 18:04:46 by dshults          ###   ########.fr       */
+/*   Updated: 2018/11/08 11:53:17 by dshults          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
-
-static long		check(char *base)
-{
-	long		i;
-	long		n;
-
-	i = 0;
-	n = 0;
-	if (base[0] == '\0' || base[1] == '\0')
-		return (0);
-	while (base[i])
-	{
-		n = i + 1;
-		if (base[i] == '+' || base[i] == '-')
-			return (0);
-		if (base[i] < ' ' || base[i] > '~')
-			return (0);
-		while (base[n])
-		{
-			if (base[i] == base[n])
-				return (0);
-			n++;
-		}
-		i++;
-	}
-	return (1);
-}
 
 /*
 ** ! Only for ft_printf !
@@ -45,30 +18,12 @@ static long		check(char *base)
 ** No support for signed negative ones
 */
 
-char			*ft_nbrconvert_base(size_t nbr, char *base)
+int    num_length(size_t value, int base)
 {
-	long		length;
-	long		res[1000];
-	int			i;
-	char		*s;
+    int    len;
 
-	i = 0;
-	length = 0;
-	s = (char *)malloc(sizeof(s));
-	if (check(base))
-	{
-		while (base[length])
-			length++;
-		while (nbr)
-		{
-			res[i] = nbr % length;
-			nbr = nbr / length;
-			i++;
-		}
-		length = 0;
-		while (--i >= 0)
-			s[length++] = base[res[i]];
-		s[length] = '\0';
-	}
-	return (s);
+    len = 1;
+    while (value /= base)
+        len++;
+    return (len);
 }
